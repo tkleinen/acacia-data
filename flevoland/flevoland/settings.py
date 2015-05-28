ff2bcb8fc028229f001ca1b8592d6cc6606be59b
theo <tkleinen@gmail.com>
2015-05-28 23:11:22 +0200

    polymorphism app added

--- flevoland/flevoland/settings.py
+++ flevoland/flevoland/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['.acaciadata.com', 'localhost']
 
 INSTALLED_APPS = (
     'grappelli',
+    'polymorphic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
