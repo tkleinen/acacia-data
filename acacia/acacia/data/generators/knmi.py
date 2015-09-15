73c6787bf3817781be0533d7d88e17b51e079fdb
theo <tkleinen@gmail.com>
2015-09-15 09:06:08 +0200

    zipmeteo generator added for knmi zip files

--- acacia/acacia/data/generators/knmi.py
+++ acacia/acacia/data/generators/knmi.py
@@ -8,6 +8,7 @@ import urlparse
 import StringIO
 import datetime
 import numpy as np
+import zipfile
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +22,6 @@ class Meteo(Generator):
     
     #url = 'http://www.knmi.nl/klimatologie/daggegevens/getdata_uur.cgi'
     url = 'http://www.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi'
-        
     def download(self, **kwargs):
         if not 'filename' in kwargs:
             # need unique filename for incremental downloads
@@ -108,6 +108,96 @@ def datehour_parser(ymd,hours):
         print e    
 #    return np.array([datetime.datetime.strptime(a + ('0' if b == '24' else b), '%Y%m%d%H') for a,b in zip(ymd,h)])
 
+class ZipMixin(object):
+
+    def process(self,zipurl,**kwargs):
+        ''' modify kwargs to download new zip file using old url '''
+        if not 'filename' in kwargs:
+            # need unique filename for incremental downloads
+            url = kwargs.get('url','')
+            filename = 'knmi_meteo'
+            if url != '':
+                parts = urlparse.urlparse(url)
+                path = os.path.basename(os.path.dirname(parts[2]))
+                query = urlparse.parse_qs(parts[4])
+                
+                start = query.get('start',None)
+                if isinstance(start,list):
+                    start = start[0]
+                if start is not None:
+                    start = datetime.datetime.strptime(start,'%Y%m%d')
+                    kwargs['start'] = start
+
+                stns=query.get('stns',[])
+                if stns != []:
+                    stn = stns[0]
+                    filename = '%s_%s_%s'% (path,filename,stn)
+                    kwargs['url'] = self.zipurl.format(stn=stn)
+                else:
+                    filename = '%s_%s'% (path,filename)
+            kwargs['filename'] = filename + '.zip'
+        return kwargs
+    
+    def unzip(self, fil):
+        try:
+            with zipfile.ZipFile(fil,'r') as z:
+                for name in z.namelist():
+                    # first (and only) file in zip archive
+                    return z.open(name)
+        except zipfile.BadZipfile:
+            # maybe not a zip file after all?
+            return fil
+
+  
+class ZipMeteo(Meteo,ZipMixin):
+
+    zipurl = 'http://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/daggegevens/etmgeg_{stn}.zip'        
+    
+    def download(self, **kwargs):
+        kwargs = self.process(self.zipurl,**kwargs)
+        return super(Meteo, self).download(**kwargs)
+
+    def get_header(self, f):
+        # new zip files have slightly different format :(
+        header = {}
+        descr = {}
+        header['DESCRIPTION'] = descr
+        line = f.readline()
+        self.skiprows = 0
+        while line != '':
+            if line.startswith('YYYYMMDD'):
+                line = f.readline()
+                self.skiprows += 1
+                while len(line)>1:
+                    eq = line.find('=')
+                    if eq>0:
+                        key = line[:eq].strip()
+                        val = line[eq+1:].strip()
+                        descr[key]=val
+                    else:
+                        break
+                    line = f.readline()
+                    self.skiprows += 1
+                while not line.startswith('# STN,YYYYMMDD'):
+                    line = f.readline()
+                    self.skiprows += 1
+                columns = [w.strip() for w in line[2:].split(',')]
+                header['COLUMNS'] = [c for c in columns if len(c)>0]
+                line = f.readline()
+                self.skiprows += 1
+                break
+            else:
+                line = f.readline()
+                self.skiprows += 1
+        return header
+
+    def get_parameters(self, fil):
+        return super(ZipMeteo,self).get_parameters(self.unzip(fil))
+
+    def get_data(self, f, **kwargs):
+        return super(ZipMeteo,self).get_data(self.unzip(f),**kwargs)
+        
+
 class UurGegevens(Meteo):
     
     url = 'http://www.knmi.nl/klimatologie/uurgegevens/getdata_uur.cgi'
@@ -193,3 +283,10 @@ class Neerslag(Meteo):
         skiprows = self.skiprows if self.engine == 'python' else 0
         data = self.read_csv(f, header=None, skiprows = skiprows, names=names, skipinitialspace=True, comment = '#', index_col = 1, parse_dates = True)
         return data
+
+class ZipNeerslag(Neerslag):
+    pass
+
+class ZipUurGegevens(UurGegevens):
+    pass
+
