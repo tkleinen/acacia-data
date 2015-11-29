5d4a875b40ea6b6c0c74813dde70dce229605a8f
theo <tkleinen@gmail.com>
2015-11-29 19:44:22 +0100

    different shapefile

--- workspace/berging/berging/settings.py
+++ workspace/berging/berging/settings.py
@@ -89,7 +89,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-SHAPEFILE = r'/media/sf_F_DRIVE/projdirs/Texel/Shapefile_website/j5rd.shp'
+#SHAPEFILE = r'/media/sf_F_DRIVE/projdirs/Texel/Shapefile_website/j5rd.shp'
+SHAPEFILE = r'/media/sf_F_DRIVE/projdirs/Texel/Shapefile_website/join.shp'
 
 LEAFLET_CONFIG = {
     'SPATIAL_EXTENT': (3.0, 50.0, 7.5, 55),
