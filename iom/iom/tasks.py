b6ce7c2e8df0a48a2612aff1e98cc55b13f9489e
theo <tkleinen@gmail.com>
2015-12-10 10:40:55 +0100

    import_akvo management command added as celery task

--- iom/iom/tasks.py
+++ iom/iom/tasks.py
@@ -1,18 +1,13 @@
 from __future__ import absolute_import
-
+from iom.management.commands import import_akvo
 from celery import shared_task
-
-
-@shared_task
-def add(x, y):
-    return x + y
-
+from django.conf import settings
 
 @shared_task
-def mul(x, y):
-    return x * y
-
+def import_Akvo(user):
+    command = import_akvo.Command()
+    command.execute(akvo=settings.AKVOFLOW_ID,cartodb=settings.CARTODB_ID,proj=1,user=user)
 
 @shared_task
-def xsum(numbers):
-    return sum(numbers)
+def import_Devices():
+    pass
