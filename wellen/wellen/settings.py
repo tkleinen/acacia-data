27850f5c6a399f90991dcb5d5b9f2ba15f1cd16a
theo <tkleinen@gmail.com>
2016-02-20 13:41:58 +0100

    passwords and keys removed

--- wellen/wellen/settings.py
+++ wellen/wellen/settings.py
@@ -16,9 +16,6 @@ os.sys.path.append('/home/theo/acaciadata.com/acacia')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's@od5ww0)1)h)(wvue!ht^$0)z(7=$75rv^1slu3g1rxz2$9br'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -75,20 +72,6 @@ ROOT_URLCONF = 'wellen.urls'
 WSGI_APPLICATION = 'wellen.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.mysql',
-        'NAME': 'wellen',                      # Or path to database file if using sqlite3.
-        'USER': 'acacia',                      # Not used with sqlite3.
-        'PASSWORD': 'Beaumont1',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -134,13 +117,7 @@ INSTALLED_APPS += ('kombu.transport.django','djcelery',)
 # registration stuff
 ACCOUNT_ACTIVATION_DAYS = 7
 LOGIN_REDIRECT_URL = '/data/'
-
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='tkleinen@gmail.com'
-EMAIL_HOST_PASSWORD='pw4Gmail'
-EMAIL_USE_TLS = True
-
+    
 LOGGING_ROOT = os.path.join(BASE_DIR, 'logs')
 
 # Logging
@@ -206,3 +183,5 @@ LOGGING = {
         },
     },
 }
+
+from secrets import *
