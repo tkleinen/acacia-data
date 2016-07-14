43fe5eee1a3e93aa7215bb8ececcc906d0c6b0a9
theo <tkleinen@gmail.com>
2016-07-14 21:24:37 +0100

    complete revision

--- delft/delft/management/commands/diff.py
+++ delft/delft/management/commands/diff.py
@@ -4,8 +4,7 @@ Created on Dec 6, 2014
 @author: theo
 '''
 from django.core.management.base import BaseCommand
-from acacia.data.models import Series
-from delft.models import ManualSeries
+from acacia.data.models import Series, ManualSeries
 import pandas as pd
 
 class Command(BaseCommand):
@@ -13,18 +12,37 @@ class Command(BaseCommand):
     help = 'Vergelijk standen en handpeilingen'
                         
     def handle(self, *args, **options):
-        print 'filter,tijdstip,stand,handpeiling'
+        dfall = None
         for h in ManualSeries.objects.all():
-            hand = pd.DataFrame(h.to_pandas())
+            hand = h.to_pandas()
             mloc = h.meetlocatie()
+            print mloc
             try:
                 name = '%s COMP' % mloc.name
                 ser = Series.objects.get(name = name)
+                try:
+                    if ser.mlocatie != mloc:
+                        ser.mlocatie = mloc
+                        ser.save()
+                except Exception as e:
+                    print e
+                    continue
             except Series.DoesNotExist:
                 print 'Series', name, 'not found' 
-            data = pd.DataFrame(ser.to_pandas())
-            inter = pd.concat([data, hand]).sort_index().interpolate(method='time').groupby(level=0).mean()
-            for p in h.datapoints.all().order_by('date'):
-                row = inter.loc[p.date]
-                print mloc.name,',',p.date,',',row.values[0],',',row.values[1]
-                
+                continue
+            
+            data = ser.to_pandas()
+
+            left,right=data.align(hand)
+            data = left.interpolate(method='time')
+            data = data.reindex(hand.index)
+            verschil = data - hand
+            df = pd.DataFrame({'locatie': str(mloc), 'data': data, 'hand': hand, 'verschil': verschil})
+            df.dropna(inplace=True)
+
+            if dfall is None:
+                dfall = df
+            else:
+                dfall = dfall.append(df)
+            dfall.to_csv('diff.csv')
+            
